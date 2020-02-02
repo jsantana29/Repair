7accbfcf703e8d1399564f8f42c098a0afed78ca
Diego Vera <woxzii@gmail.com>
2020-02-02 13:02:11 -0500

    Solved death check script picking up child colliders

--- Assets/Scripts/Player Scripts/CheckDeath.cs
+++ Assets/Scripts/Player Scripts/CheckDeath.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public class CheckDeath : MonoBehaviour
 {
+    public GameObject Target_Entity;
     private PlayerStatuses status;
-    public Collider2D Collider;
+    private Collider2D targetCollider;
+    private Collider2D objectCollider;
 
     void Start()
     {
-        status = FindObjectOfType<PlayerStatuses>();
+        status = getObjectOfType<PlayerStatuses>(Target_Entity);
+        targetCollider = getObjectOfType<Collider2D>(Target_Entity);
+        objectCollider = getObjectOfType<Collider2D>(gameObject);
+    }
+
+    // finds component from target gameobject, returning the first component found with the given type
+    private T getObjectOfType<T>(GameObject from)
+    {
+        if (from.transform.GetComponent<T>() != null)
+        {
+            return Target_Entity.transform.GetComponent<T>();
+        }
+        else
+        {
+            foreach (Transform child in from.transform)
+            {
+                if (child.GetComponent<T>() != null)
+                {
+                    return child.GetComponent<T>();
+                }
+            }
+        }
+        return default(T);
     }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if(collider.name == Collider.name && collider.tag == Collider.tag)
+        //Debug.Log($"target collider: {targetCollider.name}, object collider: {objectCollider.name}, status: {status.name}, param {collider.name}");
+        if (collider.name == targetCollider.name && collider.tag == targetCollider.tag)
         {
             status.isAlive = false;
-            Debug.Log("Test");
+            Debug.Log("Dead");
         }
     }
 }
