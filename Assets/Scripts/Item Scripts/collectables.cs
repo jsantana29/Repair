d56e97b82ed0f419872b127da3570346ad1ba2a4
schmibb@gmail.com <schmibb@gmail.com>
2020-02-01 19:08:21 -0500

    able to check if Matty collided with an item

--- Assets/Scripts/Item Scripts/collectables.cs
+++ Assets/Scripts/Item Scripts/collectables.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class collectables : MonoBehaviour
 {
+    //item dimensions
+    private int xWidth;
+    private int yHeight;
+
+    //item statuses
+    private bool hasCollected;
+    
+    //item generation
+    public Collider2D collider;
+    private Collider2D itemCollider;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        itemCollider = FindObjectOfType<Collider2D>();
+        hasCollected = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void OnTriggerEnter2D(Collider2D passedCollider) {
+        if (passedCollider.name == collider.name && passedCollider.tag == collider.tag) {
+            hasCollected = true;
+            Debug.Log($"item pickup: {itemCollider.name}");
+        }
     }
 }
